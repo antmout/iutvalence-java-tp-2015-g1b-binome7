27521cd131d27091aa5c7f7746f644cc70da29ad
chayc <cocosar07370@gmail.com>
2015-04-02 12:31:52 +0200

    Ajout de la classe Case.

---
+++ src/fr/iutvalence/ardechois/anerouge/Case.java
@@ -0,0 +1,12 @@
+package fr.iutvalence.ardechois.anerouge;
+
+/**
+ * Représentation d'une case de la grille.
+ * 
+ * @author chayc
+ */
+
+public class Case
+{
+
+}
--- src/fr/iutvalence/ardechois/anerouge/Grille.java
+++ src/fr/iutvalence/ardechois/anerouge/Grille.java
@@ -4,16 +4,36 @@ package fr.iutvalence.ardechois.anerouge;
  * Représentation de la grille du jeu.
  * 
  * @author chayc
- * 
  */
 
 public class Grille
 {
+	public static final int NOMBRE_COLONNES_DEFAUT = 10;
+	public static final int NOMBRE_LIGNES_DEFAUT = 10;
+	
+	private Case[][] grille;
+	private int nombreColonnes;
+	private int nombreLignes;
+	
 	/**
 	 * Initialise une grille <b>vide</b>.
 	 */
 	public Grille()
 	{
-		
+		this(Grille.NOMBRE_COLONNES_DEFAUT, Grille.NOMBRE_LIGNES_DEFAUT);
+	}
+
+	/**
+	 * Initialise une grille avec un nombre de colonnes et de lignes.
+	 * 
+	 * @param nombreColonnes
+	 * @param nombreLignes
+	 */
+	public Grille(int nombreColonnes, int nombreLignes)
+	{
+		super();
+		this.nombreColonnes = nombreColonnes;
+		this.nombreLignes = nombreLignes;
+		grille = new Case[this.nombreColonnes][this.nombreLignes];
 	}
 }
