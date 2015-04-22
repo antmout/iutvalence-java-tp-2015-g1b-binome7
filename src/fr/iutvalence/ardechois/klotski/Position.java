90ca2eade1143875ca4da6916cbbd6c07790e591
Anthony Gelibert <anthony.gelibert@me.com>
2015-04-23 00:45:32 +0200

    Remove useless constructor and constants.
    As your position are immutables, you will never build "default"
    position.

--- src/fr/iutvalence/ardechois/klotski/Position.java
+++ src/fr/iutvalence/ardechois/klotski/Position.java
@@ -8,51 +8,25 @@ package fr.iutvalence.ardechois.klotski;
  */
 public class Position
 {
-	/** Default x position. */
-	public static final int DEFAULT_X = 0;
-	/** Default y position. */
-	public static final int DEFAULT_Y = 0;
-
 	/** X of the position. */
 	private final int x;
 	/** Y of the position. */
 	private final int y;
 
-	/**
-	 * Create a position with default x and y.
-	 */
-	public Position()
-	{
-		this(DEFAULT_X, DEFAULT_Y);
-	}
-
-	/**
-	 * Create a position with given x and y.
-	 * 
-	 * @param x
-	 * @param y
-	 */
+	/** Create a position with given x and y. */
 	public Position(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
 	}
 
-	/**
-	 * Get x of the position.
-	 * 
-	 * @return x
-	 */
+	/** Get x of the position. */
 	public int getX()
 	{
 		return this.x;
 	}
 
-	/**
-	 * Get y of the position.
-	 * 
-	 * @return y
-	 */
+	/** Get y of the position. */
 	public int getY()
 	{
 		return this.y;
