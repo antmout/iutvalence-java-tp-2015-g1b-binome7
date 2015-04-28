65931b4d6eb4f4c15168e4d12d0275e66a8862e0
Anthony Gelibert <anthony.gelibert@me.com>
2015-04-28 10:58:31 +0200

    GameLauncher.java: use AutoCloseable nature of Scanner.

--- src/fr/iutvalence/ardechois/klotski/GameLauncher.java
+++ src/fr/iutvalence/ardechois/klotski/GameLauncher.java
@@ -4,28 +4,22 @@ import java.util.Scanner;
 
 /**
  * Allow to launch the application.
- * 
+ *
  * @author chayc
  * @version 0.04
  */
-public class GameLauncher
-{
-	/* TODO JAVADOC. */
-	private GameLauncher() { /* CAN'T HAPPEN */ }
+public class GameLauncher {
+    /** Private constructor. */
+    private GameLauncher() { /* CAN'T HAPPEN */ }
 
-	/** Main method, start of the application. */
-	public static void main(String[] args)
-	{
-		String playerName;
-		
-		Scanner playerScanner = new Scanner(System.in);
-		
-		System.out.print("Entrer votre nom: ");
-		playerName = playerScanner.nextLine();
-		
-		Klotski game = new Klotski(playerName);
-		
-		playerScanner.close();
-		game.start();
-	}
+    /** Main method, start of the application. */
+    public static void main(String[] args) {
+        try (Scanner playerScanner = new Scanner(System.in)) {
+            System.out.print("Entrer votre nom: ");
+            String playerName = playerScanner.nextLine();
+
+            Klotski game = new Klotski(playerName);
+            game.start();
+        }
+    }
 }
